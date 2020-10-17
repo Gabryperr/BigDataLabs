02987d7ac5b64f3e65d2c8a593926cd0e81df084
Gabriele Perrone <lele.gabri1997@gmail.com>
2020-10-17 21:24:38 +0200

    finished BigramCount

---
+++ lab1/BiGramCount/runOnCluster.sh
@@ -0,0 +1,19 @@
+# Remove folders of the previous run
+#hdfs dfs -rm -r example_data
+hdfs dfs -rm -r BiGramCount
+
+# Put input data collection into hdfs
+#hdfs dfs -put example_data
+
+
+# Run application
+hadoop jar BiGramCount-1.0.0.jar it.polito.bigdata.hadoop.DriverBigData 10 /data/students/bigdata-01QYD/Lab1/finefoods_text.txt BiGramCount 
+
+# Get result
+rm BiGramCount.txt
+hdfs dfs -getmerge BiGramCount BiGramCount.txt
+sort -k2,2nr -o BiGramCount.txt BiGramCount.txt
+
+
+
+
